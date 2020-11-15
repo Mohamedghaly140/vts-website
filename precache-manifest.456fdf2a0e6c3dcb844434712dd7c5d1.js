a826fc1a750fe0efece1be0eda3994d3ac353b1c
mohamedghaly140 <mohamedghaly140@gmail.com>
2020-11-15 16:45:59 +0200

    Updates

---
+++ precache-manifest.456fdf2a0e6c3dcb844434712dd7c5d1.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca7e16f3860dddc0fc9c594dfb8e49a",
+    "url": "/vts-website/index.html"
+  },
+  {
+    "revision": "8e3479dd3d734de13a89",
+    "url": "/vts-website/static/css/2.b14f1a68.chunk.css"
+  },
+  {
+    "revision": "2ab5a1724e780534d2ab",
+    "url": "/vts-website/static/css/main.819cdfb9.chunk.css"
+  },
+  {
+    "revision": "8e3479dd3d734de13a89",
+    "url": "/vts-website/static/js/2.03b2edeb.chunk.js"
+  },
+  {
+    "revision": "aff3c946005f82e0f6439b48aa21655e",
+    "url": "/vts-website/static/js/2.03b2edeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ab5a1724e780534d2ab",
+    "url": "/vts-website/static/js/main.1e928f31.chunk.js"
+  },
+  {
+    "revision": "eb2ab5c968f8de4159a5",
+    "url": "/vts-website/static/js/runtime-main.f7d400e3.js"
+  },
+  {
+    "revision": "bc88edea8e58e578fa39bcf960828487",
+    "url": "/vts-website/static/media/Digital Revolution with Digital Technology.bc88edea.mp4"
+  },
+  {
+    "revision": "cbead60e2bab75e85f872593fb083203",
+    "url": "/vts-website/static/media/Image.cbead60e.png"
+  },
+  {
+    "revision": "11e988f73e954459060f251ad4d0dfcd",
+    "url": "/vts-website/static/media/accurate forecast.11e988f7.png"
+  },
+  {
+    "revision": "430d10a2b2059e1cc1f1e3f567c75e1c",
+    "url": "/vts-website/static/media/background about.430d10a2.png"
+  },
+  {
+    "revision": "c1365bbcf5c011452c17eef4e57a90a9",
+    "url": "/vts-website/static/media/background.c1365bbc.png"
+  },
+  {
+    "revision": "8d681733120dd742327f456591cb05a0",
+    "url": "/vts-website/static/media/bu and trch.8d681733.png"
+  },
+  {
+    "revision": "43a7e19b0e6e760d21bb3db1732ad2ec",
+    "url": "/vts-website/static/media/bus several.43a7e19b.png"
+  },
+  {
+    "revision": "aee567f6be28230e70b50c525ca039af",
+    "url": "/vts-website/static/media/business erp.aee567f6.png"
+  },
+  {
+    "revision": "68865f76f0cccd6a2e209095fb6de3bd",
+    "url": "/vts-website/static/media/crm.68865f76.png"
+  },
+  {
+    "revision": "99705a41290a2056d2f8504544c856ec",
+    "url": "/vts-website/static/media/erp.99705a41.png"
+  },
+  {
+    "revision": "f4988741f82eb4fdb777522aaf10a18b",
+    "url": "/vts-website/static/media/feature.f4988741.png"
+  },
+  {
+    "revision": "648605b9df9917bf501aaaa545e58043",
+    "url": "/vts-website/static/media/hemi-head.648605b9.ttf"
+  },
+  {
+    "revision": "27779e6cf1432beadc45540159abbb30",
+    "url": "/vts-website/static/media/internet iot.27779e6c.png"
+  },
+  {
+    "revision": "b6ff97bc0333321b46747f56859b2792",
+    "url": "/vts-website/static/media/internet iot1.b6ff97bc.png"
+  },
+  {
+    "revision": "6a94a59acfca0cd9bdbfc59a04ff1af1",
+    "url": "/vts-website/static/media/internet3.6a94a59a.png"
+  },
+  {
+    "revision": "02c61d25c2051f51b824c031f49ad4f5",
+    "url": "/vts-website/static/media/it constantly.02c61d25.png"
+  },
+  {
+    "revision": "798053cee6e86c37ab182621fc86c374",
+    "url": "/vts-website/static/media/more tools - less efforts.798053ce.png"
+  },
+  {
+    "revision": "da128442841ed6595f3b516b2ffea930",
+    "url": "/vts-website/static/media/planning erp.da128442.png"
+  },
+  {
+    "revision": "2e599fefccf47aec5653a9cbeb28b4c3",
+    "url": "/vts-website/static/media/profile.2e599fef.png"
+  },
+  {
+    "revision": "a672e898018bfd77edde6c3c348e6449",
+    "url": "/vts-website/static/media/smart iot.a672e898.png"
+  },
+  {
+    "revision": "8e49384cae0205c35a07aa145f364938",
+    "url": "/vts-website/static/media/smart iot2.8e49384c.png"
+  },
+  {
+    "revision": "e4f27aca11528f5874b68c98b830cd08",
+    "url": "/vts-website/static/media/usxe crm.e4f27aca.png"
+  },
+  {
+    "revision": "43dbdcab2c3b7217e790e9dbe8084815",
+    "url": "/vts-website/static/media/vts erp.43dbdcab.png"
+  },
+  {
+    "revision": "e5b159359d725f628e0269deb9df90e1",
+    "url": "/vts-website/static/media/wave.e5b15935.png"
+  }
+]);
