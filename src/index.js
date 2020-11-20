4780f1bc3b905fbb179ee7a6b4fb29954ff169a8
mohamedghaly140 <mohamedghaly140@gmail.com>
2020-11-20 17:21:41 +0200

    adjust baseurl

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-	<BrowserRouter basename='/vts-website'>
+	<BrowserRouter>
 		<App />
 	</BrowserRouter>,
 	document.getElementById('root')
